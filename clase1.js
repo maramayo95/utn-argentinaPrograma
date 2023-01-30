fc4d55e5e657ffebe3f471fbcd709aca826509f7
Matias Aramayo <matiasaramayo@hotmail.com>
2023-01-30 19:20:58 -0300

    Resolución Ejercicios de la Clase 1

---
+++ clase1.js
@@ -0,0 +1,26 @@
+//Ejercicio 1 
+
+let auto = "Corolla";
+//console.log(auto)
+
+// Ejercicio 2 
+auto = "C4";
+console.log(auto);
+
+// Ejercicio 3
+const nombre = "Matias";
+const apellido = "Aramayo";
+console.log(`${nombre} ${apellido}`);
+
+// Ejercicio 4 
+const fruta = "naranja";
+
+// Ejercicio 5 
+const tipoDeLaVariableFruta = typeof fruta;
+console.log(tipoDeLaVariableFruta)
+
+// Ejercicio 6 
+console.log(typeof auto)
+console.log(typeof nombre)
+console.log(typeof apellido)
+
